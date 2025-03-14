3ebd4e396992368167af570b4487a9bce18995d4
Arian <ariannasiri86@gmail.com>
2025-03-14 17:12:56 +0330

    Fix cmd for activate_script that contain spaces

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pav",
-    version="1.2.2",
+    version="1.2.3",
     author="Arian",
     author_email="ariannasiri86@gmail.com",
     description="PAV is a Python library with features to easily work with venv",
